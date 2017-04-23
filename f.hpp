73d4362c3f547c1579bef20e8421db857c56880c
ethihsy <yshihte@gmail.com>
2017-04-23 23:36:25 +0200

    arr

--- f.hpp
+++ f.hpp
@@ -5,122 +5,243 @@
 #include <random>
 #include <fstream>
 #include <cstring>
-
+#include <boost/variant.hpp>
+#include <boost/variant/multivisitors.hpp>
+#include <boost/variant/variant_fwd.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
 using namespace std;
+//using namespace cv;
 
+const int v_d = 392;
+const int h_d = 200;
+const int n_s = 1;
+const int m_s = 1;
+
+const int max_epoch = 1;
+const int batch = 1;
 double alpha = 3e-5;
 double _gamma = 0.9;
 
-const int max_epoch = 1;
-const int batch = 4;
-const int n_s = 1;
-int m_s = 1;
-bool test=false;
+struct mtx{
+	double _v[batch][v_d];
+	double _a[batch][v_d];
 
+	void reset(){
+		for (int i=0; i<batch; i++)
+			for (int j=0; j<v_d; j++)
+				_a[i][j] = 1;
+		memset(_v, 0, sizeof(_v));
+	}
+};
+template<int m>
+struct mtx_s{
+	double _v[batch][h_d];				double _a[batch][h_d];
+	double _s[batch][n_s][h_d+1];		double _as[batch][n_s][h_d+1];
+	double _sy[batch][n_s][m];			double _asy[batch][n_s][m];
+
+	void reset(){
+		memset(_v, 0, sizeof(_v));		memset(_a, 0, sizeof(_a));
+		memset(_as, 0, sizeof(_as));	memset(_sy, 0, sizeof(_sy));
+	}
+};
+template<int r, int c>
+struct mtx_w{
+	double _v[r][c+1];
+	double _a[r][c+1];
+	double _m[r][c+1];
+	
+	void init(){
+		for (int i=0; i<r; i++)
+			for (int j=0; j<c+1; j++)
+				_v[i][j] = (double)rand()/RAND_MAX - 0.5;
+		memset(_m, 0, sizeof(_m));
+	}
+	void reset(){
+		memset(_a, 0, sizeof(_a));
+	}
+};
+
+
+//	0_pass	1_gumbel	2_likelihood
 template <typename T, typename Ts>
 void sample(T &h, Ts &sh){
-	int m = h.front().size();
 	switch(m_s){
 		case 0:
 			for (int k=0; k<batch; k++){
-				for (int i=0; i<m; i++)
-					sh[k][i] = h[k][i];
-				sh[k][m] = 1;
+				for (int j=0; j<n_s; j++){
+					for (int i=0; i<h_d; i++)
+						sh[k][j][i] = h[k][i];
+					sh[k][j][h_d] = 1;
+				}
 			}
 			break;
 		case 1:
 			double u, p1, p0;
 			for (int k=0; k<batch; k++){
-				for (int i=0; i<m; i++){
-					u = -log( -log((double)rand()/RAND_MAX));	p1 = exp( log(h[k][i])+u );
-					u = -log( -log((double)rand()/RAND_MAX));	p0 = exp( log(1.-h[k][i])+u );
-					sh[k][i] = p1 / (p1+p0);
+				for (int j=0; j<n_s; j++){
+					for (int i=0; i<h_d; i++){
+						u = -log( -log((double)rand()/RAND_MAX));	p1 = exp( log(h[k][i])+u );
+						u = -log( -log((double)rand()/RAND_MAX));	p0 = exp( log(1.-h[k][i])+u );
+						sh[k][j][i] = p1 / (p1+p0);
+					}
+					sh[k][j][h_d] = 1;
 				}
-				sh[k][m] = 1;
 			}
 			break;
 		case 2:
 			for (int k=0; k<batch; k++){
-				for (int i=0; i<m; i++)
-					sh[k][i] = (double)rand()/RAND_MAX > h[k][i] ? 1 : 0;
-				sh[k][m] = 1;
+				for (int j=0; j<n_s; j++){
+					for (int i=0; i<h_d; i++)
+						sh[k][j][i] = (double)rand()/RAND_MAX > h[k][i] ? 1 : 0;
+					sh[k][j][h_d] = 1;
+				}
 			}
 			break;
 		default:	cout << "sample wrong" << endl;
 	}
 }
-template<typename Ts, typename T, typename Ty>
-void a_sample(Ts &ash, Ts &sh, T &ah, T &h, Ty &y){
-	switch(m_s){
-		case 0:
-			for (int k=0; k<batch; k++)
-				for (int i=0; i<ah.front().size(); i++)
-					ah[k][i] += ash[k][i];
-			break;
-		case 1:
-			for (int k=0; k<batch; k++)
-				for (int i=0; i<ah.front().size(); i++)
-					ah[k][i] += ash[k][i] * sh[k][i]*(1.-sh[k][i])/h[k][i];
-			break;
-		case 2:
-			for (int k=0; k<batch; k++)
-				for (int i=0; i<ah.front().size(); i++)
-					ah[k][i] *= y[k][i] / h[k][i];
-			break;
-		default:	cout << "a_sample wrong" << endl;
-	}
-}
-template <typename Ts, typename Th>
-void avg_s(Ts &rh, Th &h){
-	for (int j=0; j<n_s; j++)
-		for (int k=0; k<sizeof(h)/8; k++)
-			h[k] += rh[j][k]/n_s;
-}
 template<typename Tx, typename Tw, typename Ty>
 void feed_forward(Tx &x, Tw &w, Ty &y){
+	int n=sizeof(w[0])/sizeof(double);
+	int m=sizeof(w)/sizeof(double)/n;
 	for (int k=0; k<batch; k++){
-		for (int i=0; i<w.size(); i++){
-			for (int j=0; j<w.front().size(); j++)
+		for (int i=0; i<m; i++){
+			for (int j=0; j<n; j++)
 				y[k][i] += x[k][j] * w[i][j];
-			y[k][i] = 1. / (1.+exp(-y[k][i]));
+			y[k][i] = 1.0 / (1.0+exp(-y[k][i]));
+		}
+	}
+}
+/*
+template<typename Tx, typename Tw, typename Ty, typename Tsy>
+void s_feed_forward(T &x, T &w, T &y, T &sy){
+	int n=sizeof(w[0])/sizeof(double);
+	int m=sizeof(w)/sizeof(double)/n;
+	for (int k=0; k<batch; k++){
+		for (int i=0; i<m; i++){
+			for (int l=0; l<n_s; l++){
+				for (int j=0; j<n; j++)
+					sy[k][l][i] += x[k][l][j] * w[i][j];
+				sy[k][l][i] = 1.0 / (1.0+exp(-sy[k][l][i]));
+				y[k][i] += sy[k][l][i]/n_s;
+			}
+		}
+	}
+}
+*/
+
+struct s_feed_forward: public boost::static_visitor<>{
+	template<typename Tx, typename Tw, typename Ty>
+	void operator()(Tx &x, Tw &w, Ty &y){
+	int n=sizeof(w[0])/sizeof(double);
+	int m=sizeof(w)/sizeof(double)/n;
+	for (int k=0; k<batch; k++){
+		for (int i=0; i<m; i++){
+			for (int l=0; l<n_s; l++){
+				for (int j=0; j<n; j++)
+					sy[k][l][i] += x[k][l][j] * w[i][j];
+				sy[k][l][i] = 1.0 / (1.0+exp(-sy[k][l][i]));
+				y[k][i] += sy[k][l][i]/n_s;
+			}
 		}
 	}
 }
 template<typename T, typename Tl>
 void loss_function(T &y, T &ay, Tl &l){
+	int n=sizeof(y)/sizeof(double)/batch;
 	for (int k=0; k<batch; k++){
-		for (int i=0; i<y.front().size(); i++){
-			ay[k][i] = -ay[k][i] * (l[k][i]/y[k][i] - (1.-l[k][i])/(1.-y[k][i]));
-			l[k][i] = -(l[k][i]*log(y[k][i]) + (1.-l[k][i])*log(1.-y[k][i]));
+		for (int i=0; i<n; i++){
+			ay[k][i] = -ay[k][i] * (l[k][i]/y[k][i] - (1.0-l[k][i])/(1.0-y[k][i]));
+			l[k][i] = -(l[k][i]*log(y[k][i]) + (1.0-l[k][i])*log(1.0-y[k][i]));
 		}
 	}
 }
 template<typename Tx, typename Tw, typename Ty>
-void back_prop_w(Ty &ay, Ty &y, Tw &aw, Tx &x){ 
-	double ar;
+void back_prop_w(Ty &ay, Ty &y, Tw &aw, Tx &x){
+	int n=sizeof(aw[0])/sizeof(double);
+	int m=sizeof(aw)/sizeof(double)/n;
 	for (int k=0; k<batch; k++){
-		for (int i=0; i<aw.size(); i++){
-			ar = ay[k][i] * y[k][i]*(1.-y[k][i]);
-			for (int j=0; j<aw.front().size(); j++)
-				aw[i][j] += ar * x[k][j];
+		for (int i=0; i<m; i++){
+			ay[k][i] *= y[k][i]*(1.0-y[k][i]);
+			for (int j=0; j<n; j++)
+				aw[i][j] += ay[k][i] * x[k][j];
 		}
 	}
 }
-template<typename Tx, typename Tw, typename Ty>
-void back_prop_x(Ty &ay, Ty y, Tw &w, Tx &ax){
-	double ar;
+template<typename Tx, typename Tw, typename Ty, typename Tsy>
+void s_back_prop(Ty &ay, Ty &y, Tw &aw, Tw &w, Tx &ax, Tx &x, Tsy &asy, Tsy &sy){ 
+	int n=sizeof(aw[0])/sizeof(double);
+	int m=sizeof(aw)/sizeof(double)/n;
 	for (int k=0; k<batch; k++){
-		for (int i=0; i<y.front().size(); i++){
-			ar = ay[k][i] * y[k][i]*(1.-y[k][i]);
-			for (int j=0; j<ax.front().size(); j++)
-				ax[k][j] +=  ar * w[i][j];
+		for (int i=0; i<m; i++){
+			for (int l=0; l<n_s; l++){
+				asy[k][l][i] = ay[k][i] / n_s;
+				asy[k][l][i] *= sy[k][l][i] * (1-sy[k][l][i]);
+				for (int j=0; j<n; j++){
+					aw[i][j] += asy[k][l][i] * x[k][l][j];
+					ax[k][l][j] += asy[k][l][i] * w[i][j];
+				}
+			}
 		}
+	}
+}
+/*
+template<typename Tx, typename Tw, typename Ty>
+void s_back_prop(Ty &ay, Ty &y, Tw &aw, Tw &w, Tx &ax, Tx &x){ 
+	int n=sizeof(aw[0])/sizeof(double);
+	int m=sizeof(aw)/sizeof(double)/n;
+	double ty, ts;
+	for (int k=0; k<batch; k++){
+		for (int i=0; i<m; i++){
+			for (int l=0; l<n_s; l++){
+				ty = 0;
+				ts = ay[k][i] / n_s;
+				for (int j=0; j<n; j++)
+					ty += x[k][l][j] * w[i][j];
+				ty = 1. / (1.+exp(-ty));
+				ts *= ty * (1-ty);
+				for (int j=0; j<n; j++){
+					aw[i][j] += ts * x[k][l][j];
+					ax[k][l][j] += ts * w[i][j];
+				}
+			}
+		}
+	}
+}
+*/
+template<typename Ts, typename T>
+void a_sample(Ts &ash, Ts &sh, T &ah, T &h){
+	switch(m_s){
+		case 0:
+			for (int k=0; k<batch; k++)
+				for (int j=0; j<n_s; j++)
+					for (int i=0; i<h_d; i++)
+						ah[k][i] += ash[k][j][i]/n_s;
+			break;
+		case 1:
+			for (int k=0; k<batch; k++)
+				for (int j=0; j<n_s; j++)
+					for (int i=0; i<h_d; i++)
+						ah[k][i] += ash[k][j][i] * sh[k][j][i]*(1.-sh[k][j][i])/h[k][i];
+			break;
+			/*
+		case 2:
+			for (int k=0; k<batch; k++)
+				for (int j=0; j<n_s; j++)
+					for (int i=0; i<h_d; i++)
+						ah[k][i] *= y[k][i] / h[k][i];
+			break;
+			*/
+		default:	cout << "a_sample wrong" << endl;
 	}
 }
 template<typename T>
 void optimizer(T &w, T &aw, T &vaw){
-	for (int i=0; i<w.size(); i++){
-		for (int j=0; j<w.front().size(); j++){
+	int n=sizeof(w[0])/sizeof(double);
+	int m=sizeof(w)/sizeof(double)/n;
+	for (int i=0; i<m; i++){
+		for (int j=0; j<n; j++){
 			aw[i][j] /= batch;
 			vaw[i][j] = _gamma*vaw[i][j] + alpha*aw[i][j];
 			w[i][j] = w[i][j] - vaw[i][j];
@@ -128,18 +249,20 @@ void optimizer(T &w, T &aw, T &vaw){
 	}
 }
 template<typename T>
-void write_w(T &w, int n){
+void write_w(T &w, int nn){
+	int n=sizeof(w[0])/sizeof(double);
+	int m=sizeof(w)/sizeof(double)/n;
 	fstream fs;
 	string c;
-	if (n==0)
+	if (nn==0)
 		c="w_xh1.txt";
-	else if (n==3)
+	else if (n==2)
 		c="w_h1h2.txt";
 	else
 		c="w_h2y.txt";
 	fs.open(c, ios::out);
-	for (int i=0; i<w.size(); i++)
-		for (int j=0; j<w.front().size(); j++)
+	for (int i=0; i<m; i++)
+		for (int j=0; j<n; j++)
 			fs << w[i][j] << " ";
 	fs.close();
 }
@@ -167,3 +290,74 @@ void check_data(T &x, int q){
 		cout << endl;
 	}
 }
+/*
+template<typename T, typename T2, typename T3>
+void check_gd(int lev, int pi, int pj, vector<Layer> &nn, T &x, T2 &data, T3 &loss){
+	cout << nn[lev]._a[pi][pj] << endl;;
+	double hh=1e-6;
+//	double yy=loss[0][pi];
+	double yy[v_d];	for (int i=0; i<v_d; i++)	yy[i]=loss[0][i];
+
+	for (int i=0; i<nn.size(); i++)
+		nn[i].init();
+	for (int i=0; i<batch; i++){
+		x[i][v_d] = 1;
+		for (int j=0; j<n_s; j++){
+			nn[1]._s[i][j].back() = 1;
+			nn[3]._s[i][j].back() = 1;
+		}
+		for (int j=0; j<v_d; j++){
+			x[i][j] = (double)data[i][j];
+			nn.back()._a[i][j] = 1;
+			loss[i][j] = (double)data[i][j+v_d];
+		}
+	}
+	nn[0].init_w(h_d, v_d+1);
+	nn[2].init_w(h_d, h_d+1);
+	nn[4].init_w(v_d, h_d+1);
+
+	nn[lev]._v[pi][pj] -= hh;
+	feed_forward(x, nn[0]._v, nn[1]._v);
+	for (int i=1; i<5; i+=2){
+		sample(nn[i]._v, nn[i]._s);
+		s_feed_forward(nn[i]._s, nn[i+1]._v, nn[i+2]._v);
+	}
+	loss_function(nn.back()._v, nn.back()._a, loss);
+	
+	for (int i=0; i<v_d; i++)	yy[i] -= loss[0][i];
+	for (int i=1; i<v_d; i++)	yy[0] += yy[i];
+	
+//	cout << (yy-loss[0][pi]) / hh;
+	cout << yy[0]/hh << endl;
+}
+template<typename T>
+void test(T &data, vector<Layer> &nn){
+	int max_itr = data.size()/batch;
+	double llv[max_itr];
+	double loss[batch][v_d];
+	memset(llv, 0, sizeof(llv));
+	fstream fs;
+	fs.open("test_loss.txt", ios::out);
+
+	for (int itr=0; itr<data.size(); itr+=batch){
+
+
+
+
+		for (int i=0; i<batch; i++)
+			for (int j=0; j<v_d; j++)
+				llv[itr/batch] += loss[i][j];
+		llv[itr/batch] /= batch;
+	}
+	for (int i=0; i<max_itr; i++)
+		fs << i << "\t" << llv[i] << endl;
+	fs.close();
+}
+*/
+	/*
+	Mat img(28, 28, CV_8U);
+	for (int i=0; i<28; i++)
+		for (int j=0; j<28; j++)
+			img2.at<uchar>(i,j) = dataset.test_images[0][j+i*28];
+	imshow("222",img2);
+*/
--- main.cpp
+++ main.cpp
@@ -1,172 +1,107 @@
 #include <iostream>
+#include <random>
 #include <cstdlib>
 #include <cmath>
 #include <ctime>
-#include <random>
 #include <fstream>
-//#include <omp.h>
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/highgui/highgui.hpp>
+
+#include <omp.h>
 #include "mnist/include/mnist/mnist_reader.hpp"
 #include "mnist/include/mnist/mnist_utils.hpp"
 #include "f.hpp"
 
 using namespace std;
-//using namespace cv;
-
-class Net{
-	public:
-		Net(int m, int n) : _v(m, vector<double>(n)), _a(_v), w(true){}
-		vector<vector<double>> _v;
-		vector<vector<double>> _a;
-		vector<vector<double>> _m;
-
-		void init_w(int m, int n){
-			for (int i=0; i<_v.size(); i++)
-				for (int j=0; j<_v.front().size(); j++)
-					_v[i][j] = (double)rand()/RAND_MAX - 0.5;
-			_m.assign(m, vector<double>(n, 0));
-			w = false;
-		}
-		void init(){
-			if (w){
-				for (int i=0; i<_v.size(); i++){
-					fill(_v[i].begin(), _v[i].end(), 0);
-					fill(_a[i].begin(), _a[i].end(), 0);
-				}
-			}
-			else{
-				for (int i=0; i<_v.size(); i++)
-					fill(_a[i].begin(), _a[i].end(), 0);
-			}
-		}
-	private:
-		bool w;
-};
 
 int main(){
-	auto dataset = mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>();
+	auto dataset = mnist::read_dataset<vector, vector, uint8_t, uint8_t>();
 	binarize_dataset(dataset);
 	srand(time(NULL));
 	//check_data(dataset.training_images, 11);
 
-	const int v_dim=392, h_dim=200, ly=8;
-	vector<Net> nn;
-	nn.push_back(Net(h_dim, v_dim+1));	//	wxh1
-	nn.back().init_w(h_dim, v_dim+1);
-	nn.push_back(Net(batch, h_dim));	//	h1
-	nn.push_back(Net(batch, h_dim+1));	//	sh1
-	nn.push_back(Net(h_dim, h_dim+1));	//	wh1h2
-	nn.back().init_w(h_dim, h_dim+1);
-	nn.push_back(Net(batch, h_dim));	//	h2
-	nn.push_back(Net(batch, h_dim+1));	//	sh2
-	nn.push_back(Net(v_dim, h_dim+1));	//	wh2y
-	nn.back().init_w(v_dim, h_dim+1);
-	nn.push_back(Net(batch, v_dim));	//	y
-	
-	double x[batch][v_dim+1];
+	double x[batch][v_d+1];
+	mtx_w<h_d, v_d> wxh1;	wxh1.init();
+	mtx_s<h_d> h1;
+	mtx_w<h_d, h_d> wh1h2;	wh1h2.init();
+	mtx_s<v_d> h2;
+	mtx_w<v_d, h_d> wh2y;	wh2y.init();
+	mtx y;
+	double loss[batch][v_d];
+
 	int max_itr = dataset.training_images.size()/batch;
-	double loss[batch][v_dim];
 	double gg[max_itr], gd[3];
 	double ll[max_itr];
 
 	fstream fs;
 	fs.open("train_loss.txt", ios::out);
-
+typedef boost::variant<double[batch][v_d+1], mtx, mtx_s<h_d>, mtx_s<v_d>, 
+		  					mtx_w<h_d, v_d>, mtx_w<h_d, h_d>, mtx_w<v_d, h_d>> nn;
+nn tt = wh1h2, t1=h1, t2=h2;
 //	------------------------------------------------------------------------------	
 
 	for (int epoch=0; epoch<max_epoch; epoch++){
 		shuffle(begin(dataset.training_images), end(dataset.training_images), default_random_engine(time(NULL)));
 		memset(ll, 0, sizeof(ll));
+	
 		for (int itr=0; itr<dataset.training_images.size(); itr+=batch){
-			for (int i=0; i<ly; i++)
-				nn[i].init();
+			wxh1.reset();	h1.reset();	wh1h2.reset();	h2.reset();	wh2y.reset();	y.reset();
 			for (int i=0; i<batch; i++){
-				x[i][v_dim] = 1;
-				nn[2]._v[i].back() = 1;
-				nn[5]._v[i].back() = 1;
-				for (int j=0; j<v_dim; j++){
+				x[i][v_d] = 1;
+				for (int j=0; j<v_d; j++){
 					x[i][j] = (double)dataset.training_images[i+itr][j];
-					nn.back()._a[i][j] = 1;
-					loss[i][j] = (double)dataset.training_images[i+itr][j+v_dim];
+					loss[i][j] = (double)dataset.training_images[i+itr][j+v_d];
 				}
 			}
+		
+			feed_forward(x, wxh1._v, h1._v);
+			sample(h1._v, h1._s);
+	//		s_feed_forward(h1._s, wh1h2._v, h2._v, h1._sy);
+			s_feed_forward(t1, tt, t2);
+			sample(h2._v, h2._s);
+	//		s_feed_forward(h2._s, wh2y._v, y._v, h2._sy);
 
-			feed_forward(x, nn[0]._v, nn[1]._v);
-			for (int i=1; i<5; i+=3){
-				sample(nn[i]._v, nn[i+1]._v);
-				feed_forward(nn[i+1]._v, nn[i+2]._v, nn[i+3]._v);
-			}
-			loss_function(nn.back()._v, nn.back()._a, loss);
+			loss_function(y._v, y._a, loss);
+	
+			s_back_prop(y._a, y._v, wh2y._a, wh2y._v, h2._as, h2._s, h2._asy, h2._sy);
+			a_sample(h2._as, h2._s, h2._a, h2._v);
+			s_back_prop(h2._a, h2._v, wh1h2._a, wh1h2._v, h1._as, h1._s, h1._asy, h1._sy);
+			a_sample(h1._as, h1._s, h1._a, h1._v);
+			back_prop_w(h1._a, h1._v, wxh1._a, x);
 
-			for (int i=ly-1; i>3; i-=3){
-				back_prop_w(nn[i]._a, nn[i]._v, nn[i-1]._a, nn[i-2]._v);
-				back_prop_x(nn[i]._a, nn[i]._v, nn[i-1]._v, nn[i-2]._a);
-				a_sample(nn[i-2]._a, nn[i-2]._v, nn[i-3]._a, nn[i-3]._v, nn[i]._v);
-			}
-			back_prop_w(nn[1]._a, nn[1]._v, nn[0]._a, x);
+			optimizer(wxh1._v, wxh1._a, wxh1._m);
+			optimizer(wh1h2._v, wh1h2._a, wh1h2._m);
+			optimizer(wh2y._v, wh2y._a, wh2y._m);
+	
+	//		check_gd(2,2,4,nn,x,dataset.training_images,loss);
 
-			for (int i=0; i<=ly; i+=3)
-				optimizer(nn[i]._v, nn[i]._a, nn[i]._m);
-			
 			for (int i=0; i<batch; i++)
-				for (int j=0; j<v_dim; j++)
+				for (int j=0; j<v_d; j++)
 					ll[itr/batch] += loss[i][j];
 			ll[itr/batch] /= batch;
-			memset(gd, 0, sizeof(gd));
-			for (int i=0; i<h_dim; i++){
-				for (int j=0; j<v_dim+1; j++)
-					gd[0] += nn[0]._a[i][j] * nn[0]._a[i][j];
-				for (int j=0; j<h_dim+1; j++)
-					gd[1] += nn[3]._a[i][j] * nn[3]._a[i][j];
-			}
-			for (int i=0; i<v_dim; i++)
-				for (int j=0; j<h_dim+1; j++)
-					gd[2] += nn[6]._a[i][j] * nn[6]._a[i][j];
-			gg[itr/batch] = (sqrt(gd[0])+sqrt(gd[1])+sqrt(gd[2])) / 3;
 
+			memset(gd, 0, sizeof(gd));
+			for (int i=0; i<h_d; i++){
+				for (int j=0; j<v_d+1; j++)
+					gd[0] += wxh1._a[i][j] * wxh1._a[i][j];
+				for (int j=0; j<h_d+1; j++)
+					gd[1] += wh1h2._a[i][j] * wh1h2._a[i][j];
+			}
+			for (int i=0; i<v_d; i++)
+				for (int j=0; j<h_d+1; j++)
+					gd[2] += wh2y._a[i][j] * wh2y._a[i][j];
+			gg[itr/batch] = (sqrt(gd[0])+sqrt(gd[1])+sqrt(gd[2])) / 3;
+	
 		}
 		for (int i=0; i<max_itr; i++)
 			fs << i+epoch*max_itr << "\t" << ll[i] << endl;
 		cout << "epoch: " << epoch << " is done" << endl;
 	}
 	fs.close();
-	for (int i=0; i<=6; i+=3)
-		write_w(nn[i]._v, i);
-//	------------------------------------------------------------------------------
-/*
-	if (test){
-		for (int i=0; i<dataset.test_images.size(); i++)
-			dataset.test_images[i].insert(dataset.test_images[i].begin(), 1);
-		max_itr = dataset.test_images.size()/batch;
-		double llv[max_itr];
-		memset(llv, 0, sizeof(llv));
-		fs.open("test_loss.txt", ios::out);
-		fs << "itr\tloss/step" << endl;
-		for (int itr=0; itr<dataset.test_images.size(); itr+=batch){
+//	for (int i=0; i<=4; i+=2)
+//		write_w(nn[i]._v, i);
 
-
-
-
-			for (int i=0; i<batch; i++)
-				for (int j=0; j<v_dim; j++)
-					llv[itr/batch] += loss[i][j];
-			llv[itr/batch] /= batch;
-		}
-		for (int i=0; i<max_itr; i++)
-			fs << i << "\t" << llv[i] << endl;
-		fs.close();
-	}
-*/
+//	test(dataset.test_images);
 //	------------------------------------------------------------------------------
 
-	/*
-	Mat img(28, 28, CV_8U);
-	for (int i=0; i<28; i++)
-		for (int j=0; j<28; j++)
-			img2.at<uchar>(i,j) = dataset.test_images[0][j+i*28];
-	imshow("222",img2);
-*/
 
 
 	return 0;
